4602594ef6c7b93ffb71cee394f3c5ae99d7446b
Adrified <141685834+Adrified@users.noreply.github.com>
2024-06-02 16:30:13 -0400

    Create pdf_extraction.py

---
+++ pdf_extraction.py
@@ -0,0 +1,18 @@
+done i think
+
+import fitz # pymupdf
+
+def pdf_extractor(path): # extracts text from pdf
+
+    doc = fitz.open(path, filetype = "pdf")
+    text = ""
+
+    for page in doc:
+        text += page.get_text()
+    
+    return text
+
+pdf_path = 'put path right here'
+text = pdf_extractor(pdf_path)
+print(text)
+
